b249aee1fb81bd92ba184594f26adb90b6f0cc8c
Danay1999 <dfern237@fiu.edu>
2020-02-29 16:20:04 -0500

    added info to books

--- app.py
+++ app.py
@@ -4,117 +4,106 @@ from six.moves.urllib.parse import urlencode
 from functools import wraps
 from os import environ as env
 from dotenv import load_dotenv, find_dotenv
-
 from werkzeug.exceptions import HTTPException
-
-
+from pymongo import MongoClient
+from bson import ObjectId
+from bson.json_util import dumps
+import pymongo
 import requests
-
 import json
 import constants
 
+# Database
+client = pymongo.MongoClient("mongodb+srv://bdiaz071:0312651pw@bookstore-2edyi.mongodb.net/test")
+database = client["book_info"]
+db_c = database["details"]
+db = client.book_info
 
-ENV_FILE = find_dotenv()
-if ENV_FILE:
-    load_dotenv(ENV_FILE)
 
-AUTH0_CALLBACK_URL = env.get(constants.AUTH0_CALLBACK_URL)
-AUTH0_CLIENT_ID = env.get(constants.AUTH0_CLIENT_ID)
-AUTH0_CLIENT_SECRET = env.get(constants.AUTH0_CLIENT_SECRET)
-AUTH0_DOMAIN = env.get(constants.AUTH0_DOMAIN)
-AUTH0_BASE_URL = 'https://' + AUTH0_DOMAIN
-AUTH0_AUDIENCE = env.get(constants.AUTH0_AUDIENCE)
 
-app = Flask(__name__, static_url_path="/static", static_folder="./static")
+app = Flask(__name__, static_url_path="")
 app.secret_key = constants.SECRET_KEY
 app.debug = True
 
 
-@app.errorhandler(Exception)
-def handle_auth_error(ex):
-    response = jsonify(message=str(ex))
-    response.status_code = ex.code if isinstance(ex, HTTPException) else 500
-    return response
-
-oauth = OAuth(app)
-
-auth0 = oauth.register(
-    "auth0",
-    client_id=AUTH0_CLIENT_ID,
-    client_secret=AUTH0_CLIENT_SECRET,
-    api_base_url=AUTH0_BASE_URL,
-    access_token_url=AUTH0_BASE_URL + "/oauth/token",
-    authorize_url=AUTH0_BASE_URL + "/authorize",
-    client_kwargs={"scope": "openid profile email",},
-)
-
-def requires_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        if constants.PROFILE_KEY not in session:
-            return redirect("/login")
-        return f(*args, **kwargs)
-
-    return decorated
 
 @app.route("/")
 def home():
     return render_template("/index.html")
 
-@app.route("/callback")
-def callback_handling():
-    # Handles response from token endpoint
-    auth0.authorize_access_token()
-    resp = auth0.get("userinfo")
-    userinfo = resp.json()
-
-    # Store the user information in flask session.
-    session[constants.JWT_PAYLOAD] = userinfo
-    session[constants.PROFILE_KEY] = {
-        "user_id": userinfo["sub"],
-        "name": userinfo["name"],
-        "picture": userinfo["picture"],
-    }
-    return redirect("/")
 
 @app.route("/login")
 def login():
-    return auth0.authorize_redirect(
-        redirect_uri=AUTH0_CALLBACK_URL, audience=AUTH0_AUDIENCE
-    )
-
-@app.route("/logout")
-def logout():
-    session.clear()
-    params = {"returnTo": url_for("home", _external=True), "client_id": AUTH0_CLIENT_ID}
-    return redirect(auth0.api_base_url + "/v2/logout?" + urlencode(params))
-
-
+    return render_template("/login.html")
+    
 
 
 # from GeekText.views.index import bp as index_bp
 # app.register_blueprint(index_bp)
 
-
 @app.route("/wishlist")
 def wishlist():
     return render_template("/wishlist.html")
 
-@app.route("/books")
+@app.route("/books", methods = ['GET'])
 def books():
-    return render_template("books.html")
+    try:
+        books = db.details.find()
+        return render_template("/books.html", books=books)
+    except Exception as e:
+        return dumps({'error' : str(e)})
+
+
+@app.route("/books/<link>")
+def distinctbook(link):
+    return render_template("/distinctbooks/"+link+".html")
+
 
 @app.route("/shoppingcart")
 def shoppingcart():
-    return render_template("shoppingcart.html")
+    return render_template("/shoppingcart.html")
 
 
 @app.route("/signup")
 def signup():
-    return auth0.authorize_redirect(
-        redirect_uri=AUTH0_CALLBACK_URL, audience=AUTH0_AUDIENCE
-    )
+    return render_template("/signup.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
     app.run(host="localhost", port=env.get("PORT", 5000))
+
+
+# Database functions
+# Inserts data into the collection that you want
+def insert_data(data):
+    document = db_c.insert_one(data)
+    return document.inserted_id
+
+# Updates data or creates a new one if it doesn't have the same ID
+def update_or_create(document_id, data):
+    document = db_c.update_one({'_id': ObjectId(document_id)}, {"$set": data}, upsert=True)
+    return document.acknowledged
+
+# Gets all the data of a single ID
+def get_single_data(document_id):
+    data = db_c.find_one({'_id': ObjectId(document_id)})
+    return data
+
+# Gets all the data inside the collection
+def get_multiple_data():
+    data = db_c.find()
+    return list(data)
+
+# Updates an existing data
+def update_existing(document_id, data):
+    document = db_c.update_one({'_id': ObjectId(document_id)}, {"$set": data})
+    return document.acknowledged
+
+# Removes the data
+def remove_data(document_id):
+    document = db_c.delete_one({'_id': ObjectId(document_id)})
+    return document.acknowledged
+
+
+# CLOSE DATABASE
+client.close()
