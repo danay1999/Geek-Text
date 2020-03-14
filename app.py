b4fe738f1fc8db5c7e9f4328f6ce4c8ca5b86574
Danay1999 <dfern237@fiu.edu>
2020-03-14 13:16:25 -0400

    individual pictures

--- app.py
+++ app.py
@@ -13,7 +13,9 @@ import json
 import constants
 
 # Database
-client = pymongo.MongoClient("mongodb+srv://bdiaz071:0312651pw@bookstore-2edyi.mongodb.net/test")
+client = pymongo.MongoClient(
+    "mongodb+srv://bdiaz071:0312651pw@bookstore-2edyi.mongodb.net/test"
+)
 database = client["book_info"]
 db_c = database["details"]
 db = client.book_info
@@ -21,6 +23,7 @@ db = client.book_info
 app = Flask(__name__, static_url_path="")
 app.secret_key = constants.SECRET_KEY
 app.debug = True
+
 
 @app.route("/")
 def home():
@@ -30,37 +33,34 @@ def home():
 @app.route("/login")
 def login():
     return render_template("/login.html")
-    
 
 
 # from GeekText.views.index import bp as index_bp
 # app.register_blueprint(index_bp)
 
-@app.route('/wishlist', methods=['GET', 'POST'])
+
+@app.route("/wishlist", methods=["GET", "POST"])
 def wishlist():
 
     results = db_c.find({}, {"book_name": 1, "author_name": 1})
-    return render_template('wishlist.html', results=results)
+    return render_template("wishlist.html", results=results)
 
-PyMongo.send_file("comingsoon.jpg", base='fs', version=-1, cache_for=31536000)
-
-@app.route("/books", methods = ['GET'])
+@app.route("/books", methods=["GET"])
 def books():
     try:
         books = db.details.find()
         return render_template("/books.html", books=books)
     except Exception as e:
-        return dumps({'error' : str(e)})
+        return dumps({"error": str(e)})
 
 
-@app.route("/books/<link>", methods = ['GET'])
+@app.route("/books/<link>", methods=["GET"])
 def distinctbook(link):
     try:
         books = db.details.find()
-        return render_template("/distinctbooks/"+link+".html",books=books)
+        return render_template("/distinctbooks/" + link + ".html", books=books)
     except Exception as e:
-        return dumps({'error' : str(e)})
-
+        return dumps({"error": str(e)})
 
 
 @app.route("/shoppingcart")
@@ -71,6 +71,7 @@ def shoppingcart():
 @app.route("/signup")
 def signup():
     return render_template("/signup.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
@@ -83,29 +84,36 @@ def insert_data(data):
     document = db_c.insert_one(data)
     return document.inserted_id
 
+
 # Updates data or creates a new one if it doesn't have the same ID
 def update_or_create(document_id, data):
-    document = db_c.update_one({'_id': ObjectId(document_id)}, {"$set": data}, upsert=True)
+    document = db_c.update_one(
+        {"_id": ObjectId(document_id)}, {"$set": data}, upsert=True
+    )
     return document.acknowledged
+
 
 # Gets all the data of a single ID
 def get_single_data(document_id):
-    data = db_c.find_one({'_id': ObjectId(document_id)})
+    data = db_c.find_one({"_id": ObjectId(document_id)})
     return data
+
 
 # Gets all the data inside the collection
 def get_multiple_data():
     data = db_c.find()
     return list(data)
 
+
 # Updates an existing data
 def update_existing(document_id, data):
-    document = db_c.update_one({'_id': ObjectId(document_id)}, {"$set": data})
+    document = db_c.update_one({"_id": ObjectId(document_id)}, {"$set": data})
     return document.acknowledged
+
 
 # Removes the data
 def remove_data(document_id):
-    document = db_c.delete_one({'_id': ObjectId(document_id)})
+    document = db_c.delete_one({"_id": ObjectId(document_id)})
     return document.acknowledged
 
 
