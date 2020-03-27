824adf5567e037e2c0249643971b9b1bdcc62710
cduran035 <50387976+cduran035@users.noreply.github.com>
2020-03-27 16:26:32 -0400

    Update to app.py
    Wishlist functions were added.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, redirect, session, url_for, request
+from flask import Flask, render_template, jsonify, redirect, session, url_for, request,flash
 from six.moves.urllib.parse import urlencode
 from functools import wraps
 from os import environ as env
@@ -9,6 +9,10 @@ import pymongo
 import requests
 from flask_pymongo import PyMongo
 import bcrypt
+from bson import ObjectId
+from bson.json_util import dumps
+from forms import TitleForm1, TitleForm2, TitleForm3
+import os
 
 
 # Database
@@ -18,11 +22,15 @@ client = pymongo.MongoClient(
 database = client["book_info"]
 db_c = database["details"]
 db = database["users"]
+wishlist_c = database['wish_list']  # wishlist collection.
+cart_c= database['shopping_cart']
 db_b = client.book_info
 
 app = Flask(__name__, static_url_path="")
-
 app.debug = True
+
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 
 @app.route("/")
@@ -35,11 +43,174 @@ def home():
 # app.register_blueprint(index_bp)
 
 
-@app.route("/wishlist", methods=["GET", "POST"])
-def wishlist():
 
-    results = db_c.find({}, {"book_name": 1, "author_name": 1})
-    return render_template("wishlist.html", results=results)
+@app.route('/wishlist', methods=['GET', 'POST'])
+def wishlist():
+    print("WISHLIST")
+    form1 = TitleForm1()
+    form2 = TitleForm2()
+    form3 = TitleForm3()
+    if form1.validate_on_submit():
+        flash(f'Titles Created Successfully! : {form1.title1.data}','success')
+        wishlist_c.update_one({"wishlist_id":1},{"$set":{"wishlist_title": form1.title1.data}})
+        return redirect(url_for('wishlist'))
+
+    if form2.validate_on_submit():
+        flash(f'Titles Created Successfully! :  {form2.title2.data}','success')
+        wishlist_c.update_one({"wishlist_id":2},{"$set":{"wishlist_title2": form2.title2.data}})
+        return redirect(url_for('wishlist'))
+
+    if form3.validate_on_submit():
+        flash(f'Titles Created Successfully! :  {form3.title3.data}','success')
+        wishlist_c.update_one({"wishlist_id":3},{"$set":{"wishlist_title3": form3.title3.data}})
+        return redirect(url_for('wishlist'))
+
+
+    # This needs to pass eventually the wishlist collection and not the books db_c
+    # results = wishlist_c.find(
+    #     {}, {"_id": 1, "wishlist_title": 1, "books_arr": 1, "wishlist_id":1})
+    results = wishlist_c.find({"wishlist_id": 1})
+    results2 = wishlist_c.find(
+        {}, {"_id": 1, "wishlist_title2": 1, "books_arr2": 1,"wishlist_id":1})
+
+    results3 = wishlist_c.find(
+        {}, {"_id": 1, "wishlist_title3": 1, "books_arr3": 1, "wishlist_id":1})
+
+    return render_template('wishlist.html', results=results, results2=results2, results3=results3, form1=form1, form2=form2,form3=form3)
+
+###########################################################
+#Wish list add btn.
+@app.route("/add/<list_id>/<book_id>")
+def add(list_id,book_id):
+    # Add a wishlist item.
+    print("ADDED")
+    print("LIST NUMBER: ",list_id )
+    print("BOOK ID: ",book_id)
+
+    # key = request.values.get("_id")
+    # print(key)
+    result = db_c.find_one({"_id": ObjectId(book_id)})
+    # wishlist_c.update_one({"wishlist_id": 1}, {
+    #                       "$push": {"books_arr": ObjectId(key)}})
+    # print("PRINTING RESULT: ", result)
+    if list_id == "1":
+        wishlist_c.update_one({"wishlist_id": 1}, {
+                          "$addToSet": {"books_arr": result}})
+    elif list_id == "2":
+        wishlist_c.update_one({"wishlist_id": 2}, {
+                          "$addToSet": {"books_arr2": result}})
+    else:
+        wishlist_c.update_one({"wishlist_id": 3}, {
+                          "$addToSet": {"books_arr3": result}})
+
+
+
+
+    # wishlist_c.update_one({"wishlist_id": 1}, {
+                        #   "$addToSet": {"books_arr": result}})
+    # wishlist_c.insert_one(result)
+    return redirect('/books')
+###########################################################
+# Wishlist remove btn.
+@app.route("/remove/<list_id>/<book_id>")
+def remove(list_id, book_id):
+    # Delete wishlist item.
+    print("REMOVED")
+    # key = request.values.get("_id")
+
+    # key = request.values.get("_id")
+
+    # if list_id == "1":
+
+    # wishlist_c.delete_one({"books_arr": key})
+
+    if list_id == "1":
+         wishlist_c.update_one({"wishlist_id": 1}, {
+                          "$pull": {"books_arr": {"_id": ObjectId(book_id)}}})
+    elif list_id == "2":
+         wishlist_c.update_one({"wishlist_id": 2}, {
+                          "$pull": {"books_arr2": {"_id": ObjectId(book_id)}}})
+    else:
+         wishlist_c.update_one({"wishlist_id": 3}, {
+                          "$pull": {"books_arr3": {"_id": ObjectId(book_id)}}})
+
+
+   
+    return redirect('/wishlist')
+######################################################################
+# Wishlist move btn
+@app.route("/moveBooks/<list_id>/<book_id>")
+def moveBooks(list_id, book_id):
+    
+    if list_id == "1":
+        print("LIST ID: ", list_id)
+        #Add to 2nd Array of Books
+        result = db_c.find_one({"_id": ObjectId(book_id)})
+        wishlist_c.update_one({"wishlist_id": 2}, {
+                          "$addToSet": {"books_arr2": result}})
+        #Remove from 1st Array of Books
+        wishlist_c.update_one({"wishlist_id": 1}, {
+                          "$pull": {"books_arr": {"_id":ObjectId(book_id)}}})
+    elif list_id == "2":
+        result = db_c.find_one({"_id": ObjectId(book_id)})
+        wishlist_c.update_one({"wishlist_id": 3}, {
+                          "$addToSet": {"books_arr3": result}})
+        wishlist_c.update_one({"wishlist_id": 2}, {
+                          "$pull": {"books_arr2": {"_id":ObjectId(book_id)}}})
+    else:
+        result = db_c.find_one({"_id": ObjectId(book_id)})
+        wishlist_c.update_one({"wishlist_id": 1}, {
+                          "$addToSet": {"books_arr": result}})
+        wishlist_c.update_one({"wishlist_id": 3}, {
+                          "$pull": {"books_arr3": {"_id":ObjectId(book_id)}}})
+
+    # print("BOOK ID: ", book_id)
+    return redirect('/wishlist')
+#################################################################################################
+# Wishlist move to Cart btn.
+@app.route("/moveToCart/<list_id>/<book_id>")
+def moveToCart(list_id, book_id):
+    #Find which book it is in the books collection.
+    result = db_c.find_one({"_id": ObjectId(book_id)})
+
+    #Insert into the shopping cart collection.
+    cart_c.insert_one(result)
+
+    #Remove from displaying in the wishlist.
+    if list_id == "1":
+        wishlist_c.update_one({"wishlist_id": 1}, {
+                          "$pull": {"books_arr": {"_id":ObjectId(book_id)}}})
+    elif list_id == "2":
+        wishlist_c.update_one({"wishlist_id": 2}, {
+                          "$pull": {"books_arr2": {"_id":ObjectId(book_id)}}})
+    else:
+        wishlist_c.update_one({"wishlist_id": 3}, {
+                          "$pull": {"books_arr3": {"_id":ObjectId(book_id)}}})
+
+     
+
+    return redirect('/wishlist')
+
+###################################################################################################
+# Wishlist move to cart btn.
+@app.route("/createList")
+def createList():
+    listNum= wishlist_c.count_documents({"user_id":"1"})
+
+    print("NUM OF LISTS: ",listNum)
+
+    if listNum < 3:
+        wishlist_c.insert_one({"wishlist":4, "wishlist_title":"Default Title 4","books_arr":[],"user_id":"4"})
+
+
+    return "" 
+
+###################################################################################################
+
+
+
+
+
 
 @app.route("/books", methods=["GET"])
 def books():
